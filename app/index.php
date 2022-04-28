0fbb8d35d55d99e656c43b9f0e444cc79a0ec36f
fret18 <57362241+fret18@users.noreply.github.com>
2022-04-28 09:37:32 -0400

    I hope this works

--- app/index.php
+++ app/index.php
@@ -14,9 +14,9 @@
                 $host = "localhost";
                 $port = "81";
                 $socket = "3307";
-                $user = "root";
-                $password = "";
-                $dbname = "BoozeBuddy";
+                $user = "admin";
+                $password = "PAehcD0Za9fW";
+                $dbname = "boozebuddy";
                 
                 //test connection
                 try {
@@ -28,28 +28,28 @@
                     echo 'Connection failed: ' . $e->getMessage();
                 }
                 //change these to reflect actual data
-                $sqlquery = "INSERT INTO Alcohol VALUES ( 105, 'Miller Lite2', 4, 'Lite Lager YEET', 'America Y NOT')";
-                echo 'before if statement';
-                if ($conn->query($sqlquery) === TRUE) {
-                    echo "record inserted successfully";
-                } else {
-                    echo "Error: " . $sql . "<br>" . $conn->error;
-                }
+                // $sqlquery = "INSERT INTO Alcohol VALUES ( 10, 'Miller Lite2', 4, 'Lite Lager YEET', 'America Y NOT')";
+                // echo 'before if statement';
+                // if ($conn->query($sqlquery) === TRUE) {
+                //     echo "record inserted successfully";
+                // } else {
+                //     echo "Error: " . $sql . "<br>" . $conn->error;
+                // }
             ?>
         
             <h2> Please enter information </h2>
-            <form>
+            <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
                 <label for="storeID"> Store ID: </label>
                 <br>
                 <input type="text" id="storeID">
                 <br><br>
                 <label for="price"> Price: </label>
                 <br>
-                <input type="text" id="price">
+                <input type="text" id="Price">
                 <br><br>
                 <label for="alcoholID"> Alcohol ID: </label>
                 <br>
-                <input type="text" id="alcoholID">
+                <input type="text" id="AlcID">
                 <br><br>
                 <label for="date"> Date Observed: </label>
                 <br>
@@ -57,30 +57,51 @@
                 <br><br>
                 <input type="submit" value="submit">
             </form>
-            <?php
 
-                if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        
+            <?php
                     // collect value of input field
-                    $storeID = $_REQUEST['storeID'];
-                    $price = $_REQUEST['price'];
-                    $alcoholID = $_REQUEST['alcoholID'];
-                    $date = $_REQUEST['date'];
+                    $StoreID = $_REQUEST['StoreID'];
+                    $Price = $_REQUEST['Price'];
+                    $AlcID = $_REQUEST['alcID'];
+
+                    $EnterStoreID = $_POST['$StoreID']
+                    $EnterPrice = $_POST['$Price']
+                    $EnterAlcID = $_POST['$AlcID']
+
+                    $sql = "INSERT INTO Store_Bev (StoreID) VALUES ('$EnterStoreID')"; 
                 
-                    if (empty($alcoholID) || empty($storeID) || empty($price)) {
-                        echo "data is empty";
+                    if (empty($AlcID) || empty($StoreID) || empty($Price)) {
+                        echo "data is empty<br>";
                     } else {
-                        echo $storeID;
-                        echo $price;
-                        echo $alcoholID;
-                        echo $date;
+                        if($conn->query($sql) ===TRUE) {
+                            echo "Records entered succesfully";
+                        } else {
+                            echo "ERROR: " . $sql . "<br>" . $conn->error;
+                        }
                     }
-                }
+                
 
                 // Closing the connection.
                 $conn->close();
 
             ?>
+   
+            <?php
+               
+                $sql = "SELECT storeID, price, alcID FROM Store_Bev";
+                $conn = new mysqli($host, $user, $password, $dbname);
+                $result = $conn->query($sql);
+                
+                if ($result->num_rows > 0) {
+                  // output data of each row
+                  while($row = $result->fetch_assoc()) {
+                    echo "id: " . $row["storeID"]. " - Name: " . $row["price"]. " " . $row["alcID"]. "<br>";
+                  }
+                } else {
+                  echo "0 results";
+                }
+            ?>
+       
         </main>
     </body>
 </html>
